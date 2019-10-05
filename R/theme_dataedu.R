c42271009a272d653f508c809aadfcea0e6bd349
ivelasq <ivelasq@gmail.com>
2019-10-05 11:46:26 -0700

    create theme code

---
+++ R/theme_dataedu.R
@@ -0,0 +1,25 @@
+#' Plot theme for Data Science in Education
+
+#' Libraries
+library(tidyverse)
+if (!require("extrafont")) install.packages("extrafont")
+library(extrafont)
+if(.Platform$OS.type == "windows") {
+  font_import(pattern = "ARLRDBD")
+  loadfonts(device = "win", quiet = TRUE)
+} else {
+  font_import(pattern = "Arial Rounded Bold")
+  loadfonts()
+}
+
+#' Function
+theme_dataedu <-
+  function () {
+    theme_minimal(
+      base_size = 12, 
+      base_family = "Arial Rounded MT Bold") %+replace% 
+      theme(
+        legend.background = element_rect(fill="transparent", colour=NA),
+        legend.key = element_rect(fill = "transparent", colour = NA)
+      )
+  }
