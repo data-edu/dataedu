6d7793c5bb07d61cda5f52a3dbace2d6930eb0d4
restrellado <ryan.estrellado@gmail.com>
2019-12-30 10:47:29 -0800

    Fix longitudinal data documentation

--- R/longitudinal_data.R
+++
@@ -1,6 +0,0 @@
-#' Datasets containing students with disabilities enrollment
-#'
-#' @source US Department of Education \url{https://www2.ed.gov/programs/osepidea/618-data/state-level-data-files/index.html}
-#' @format Tibble
-#' }
-"longitudinal_list"
