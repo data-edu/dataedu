39ca1e692041c979b41485e64f878090f2b2b74e
restrellado <ryan.estrellado@gmail.com>
2019-12-30 10:08:52 -0800

    Replace longitudinal list with processed dataset

---
+++ R/longitudinal_data.R
@@ -0,0 +1,6 @@
+#' Datasets containing students with disabilities enrollment
+#'
+#' @source US Department of Education \url{https://www2.ed.gov/programs/osepidea/618-data/state-level-data-files/index.html}
+#' @format Tibble
+#' }
+"longitudinal_list"
