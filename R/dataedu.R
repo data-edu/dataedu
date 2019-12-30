88d6daf54451683cbad17de3e105cbb34d7a1372
ivelasq <ivelasq@gmail.com>
2019-12-29 22:44:56 -0600

    change package documentation

--- R/dataedu.R
+++
@@ -1,8 +0,0 @@
-#' dataedu: A package for Data Science in Education Using R
-#'
-#' The dataedu package provides readers of Data Science in Education Using R 
-#' with a package with useful functions, data, and references from the book.
-#' 
-#' @docType package
-#' @name dataedu
-NULL
