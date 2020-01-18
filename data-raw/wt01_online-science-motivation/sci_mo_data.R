af085c7e78fe1980931991035b3c9df7d889899d
Isabella Velásquez <ivelasq@gmail.com>
2020-01-17 16:26:02 -0800

    editing files to reflect new structure

--- data-raw/wt01_online-science-motivation/sci_mo_data.R
+++ data-raw/wt01_online-science-motivation/sci_mo_data.R
@@ -4,7 +4,7 @@ library(synthpop)
 # # this code to prepare processed data from raw data is commented out so that
 # # only the code that turns the processed data into package data with usethis::use_data()
 
-# d <- read_csv(here::here("data", "raw", "sci_mo_data.csv"))
+# d <- read_csv(here::here("data-raw", "wt01_online-science-motivation", raw", "sci_mo_data.csv"))
 #
 # #filtering the data to only include spring 2017 data
 # d <-d %>% filter(!str_detect(course_ID, "S217"))
@@ -16,7 +16,7 @@ library(synthpop)
 #
 # d_syn <- syn(d_raw)
 #
-# write_csv(d_syn$syn, here::here("data", "processed", "sci_mo_data.csv"))
+# write_csv(d_syn$syn, here::here("data-raw", "wt01_online-science-motivation", "processed", "sci_mo_data.csv"))
 
-sci_mo_data <- read_csv(here::here("data", "processed", "sci_mo_data.csv"))
+sci_mo_data <- read_csv(here::here("data-raw", "wt01_online-science-motivation", "processed", "sci_mo_data.csv"))
 usethis::use_data(sci_mo_data, overwrite = TRUE)
