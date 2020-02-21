b72b483d13e5307688e19162f8aa9f8d11c68152
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-02-20 20:11:46 -0500

    update data

--- data-raw/wt01_online-science-motivation/online-science-motivation.R
+++ data-raw/wt01_online-science-motivation/online-science-motivation.R
@@ -70,5 +70,4 @@ usethis::use_data(pre_survey, overwrite = TRUE)
 course_minutes <- read_csv(here::here("data-raw", "wt01_online-science-motivation", "processed", "s12-course-minutes.csv"))
 usethis::use_data(course_minutes, overwrite = TRUE)
 
-sci_mo_processed <- read_csv(here::here("data-raw", "wt01_online-science-motivation", "processed", "sci_mo_processed.csv"))
-usethis::use_data(sci_mo_processed, overwrite = TRUE)
+
