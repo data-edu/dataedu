5eab9d151972eff9694f6f811bf8965fb9a7c8aa
Joshua Rosenberg <jrosen48@github.com>
2020-02-28 16:27:48 -0500

    add enrollment status and reason

--- data-raw/wt01_online-science-motivation/sci_mo_processed.R
+++ data-raw/wt01_online-science-motivation/sci_mo_processed.R
@@ -7,7 +7,9 @@ usethis::use_data(sci_mo_processed, overwrite = TRUE)
 # for sci_mo_text
 old_data_with_liwc <- read_csv(here::here("data-raw", "wt01_online-science-motivation", "raw", "sci_mo_data.csv"))
 old_data_with_liwc <- old_data_with_liwc %>% 
-  dplyr::select(student_id = student_ID, cogproc:WC, n)
+  dplyr::select(student_id = student_ID, enrollment_status, enrollment_reason, cogproc:WC, n) %>% 
+  rename(final_grade = FinalGradeCEMS,
+         time_spent = TimeSpent)
 
 sci_mo_with_text <- sci_mo_processed %>% 
   left_join(old_data_with_liwc)
