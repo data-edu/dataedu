9e00213de0c6ffb22724f066d35138ff46cd74d1
Isabella Velásquez <ivelasq@gmail.com>
2020-03-02 22:11:09 -0800

    add palette tests

---
+++ tests/testthat/helper-utils.R
@@ -0,0 +1,21 @@
+# from https://github.com/jrnold/ggthemes/blob/master/tests/testthat/helper-utils.R
+
+is_hexcolor <- function(x) {
+  pattern <- stringr::regex("^#[a-f0-9]{6}$", ignore_case = TRUE)
+  out <- stringr::str_detect(x, pattern)
+  out[is.na(out)] <- FALSE
+  out
+}
+
+expect_hexcolor <- function(object) {
+  # capture object and label
+  act <- quasi_label(rlang::enquo(object))
+  
+  valid <- is_hexcolor(act$val)
+  expect(
+    all(valid),
+    glue::glue("Not all elements of {act$lab} are hex colors.")
+  )
+  
+  invisible(act$val)
+}
---
+++ tests/testthat/test-palette_dataedu.R
@@ -0,0 +1,19 @@
+library(testthat)
+library(dataedu)
+context("test-palette_dataedu.R")
+
+test_that("dataedu_pal main works", {
+  p <- dataedu_pal()
+  expect_is(p, "function")
+  for (i in 1:5) {
+    expect_hexcolor(p(i))
+  }
+})
+
+test_that("scale_color_dataedu works", {
+  expect_is(scale_color_dataedu(), "ScaleDiscrete")
+})
+
+test_that("scale_fill_dataedu works", {
+  expect_is(scale_fill_dataedu(), "ScaleDiscrete")
+})
