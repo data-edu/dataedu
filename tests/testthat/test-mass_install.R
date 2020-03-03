d4eee327bb810efe24e198f6ef038e0a80ce0e53
Isabella Velásquez <ivelasq@gmail.com>
2020-03-02 22:51:27 -0800

    let's try this test again :D

--- tests/testthat/test-mass_install.R
+++ tests/testthat/test-mass_install.R
@@ -3,8 +3,5 @@ library(dataedu)
 context("test-mass_install.R")
 
 test_that("mass_install() works", {
-  with_mock(expect_error_free(expect_true(
-    "apaTables" %in% rownames(installed.packages())
-  )),
-  apaTables::dating_wide)
-})
+  expect_true("apaTables" %in% rownames(installed.packages()))
+}) 
