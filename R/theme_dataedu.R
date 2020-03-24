295c0e9c557afc9322cc2c147845257c046a1bf1
isabellav <isabella.velasquez@gatesfoundation.org>
2020-03-24 13:34:57 -0500

    have to add back extrafont because of windows

--- R/theme_dataedu.R
+++ R/theme_dataedu.R
@@ -7,12 +7,17 @@
 #' theme_dataedu()
 #'
 #' @details
-#' The dataedu theme uses Gills Sans Mt.
+#' The dataedu theme uses the font Gills Sans Mt which is available on Macs by default
+#' and used on Windows through the [extrafont](https://github.com/wch/extrafont) package.
+#' 
+#' To use [extrafont](https://github.com/wch/extrafont), the extrafont database is required. More information can be found on the [extrafont](https://github.com/wch/extrafont) help page.
 #' @rdname theme_dataedu
 #' @export
 theme_dataedu <- function() {
-  ggplot2::theme_minimal(
-    base_size = 12,
-    base_family = "Gill Sans MT"
-  )
+  if (.Platform$OS.type == "windows") {
+    extrafont::font_import(pattern = "GIL_____", prompt = FALSE)
+    extrafont::loadfonts(device = "win", quiet = TRUE)
+  }
+  ggplot2::theme_minimal(base_size = 12,
+                         base_family = "Gill Sans MT")
 }
