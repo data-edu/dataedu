160bcf8c575d367ffd360e2614c774117fbb5c25
Isabella Velasquez <ivelasq@gmail.com>
2025-02-15 21:45:46 -0600

    Edit test

--- tests/testthat/test-palette_dataedu.R
+++ tests/testthat/test-palette_dataedu.R
@@ -3,25 +3,25 @@ library(dataedu)
 context("test-palette_dataedu.R")
 
 test_that("dataedu_pal() main works", {
-  p <- dataedu_pal()
+  p <- dataedu:::dataedu_pal()
   expect_is(p, "function")
-  for (i in 1:5) {
+  for (i in 1:6) {
     expect_hexcolor(p(i))
   }
 })
 
 test_that("dataedu_pal() reverse works", {
-  p <- dataedu_pal(reverse = TRUE)
+  p <- dataedu:::dataedu_pal(reverse = TRUE)
   expect_is(p, "function")
-  for (i in 1:5) {
+  for (i in 1:6) {
     expect_hexcolor(p(i))
   }
 })
 
 test_that("scale_color_dataedu() works", {
-  expect_is(scale_color_dataedu(), "ScaleDiscrete")
+  expect_is(dataedu:::scale_color_dataedu(), "ScaleDiscrete")
 })
 
 test_that("scale_fill_dataedu() works", {
-  expect_is(scale_fill_dataedu(), "ScaleDiscrete")
+  expect_is(dataedu:::scale_fill_dataedu(), "ScaleDiscrete")
 })
